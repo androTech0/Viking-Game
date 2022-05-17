625a5780ab5421677c2fd5a80b42699e12cffcc5
androTech0 <khbu2017@gmail.com>
2022-05-17 19:14:56 +0300

    9

---
+++ Viking-Game/Assets/Scripts/Player/ResouresePosition.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResouresePosition : MonoBehaviour
+{
+
+    [SerializeField]
+    Animator animator;
+    public Transform[] CrystalRed;
+    int index = 0;
+    float speed = 2f;
+    int stopTime = 300;
+
+    void Start()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, CrystalRed[index].transform.position, 2f * Time.deltaTime);
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        if (index <= CrystalRed.Length - 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, CrystalRed[index].transform.position, speed * Time.deltaTime);
+            /*if (transform.position == CrystalRed[index].transform.position)
+            {
+                index += 1;
+            }*/
+        }
+    }
+   
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coll")
+        {
+            if (stopTime > 0)
+            {
+                Debug.Log("Stay - "+ stopTime);
+                animator.SetBool("forword", false);
+                animator.SetBool("Cut", true);
+                speed = 0f;
+                stopTime -= 2;
+            }
+            else
+            {
+
+                animator.SetBool("Cut", false);
+                Destroy(CrystalRed[index].gameObject);
+
+                index += 1;
+                speed = 2f;
+                stopTime = 300;
+                animator.SetBool("forword", true);
+            }
+        }
+    }
+
+    /*private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Coll")
+        {
+            index += 1;
+            speed = 2f;
+            stopTime = 5;
+
+        }
+    }
+    */
+
+}
